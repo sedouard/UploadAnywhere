e268ddc81b7cb23fdee03b12b016b6beb923c7e5
sedouard <steven.edouard1@gmail.com>
2013-12-26 19:40:07 -0800

    Initial Commit.
    This solution has 2 console apps. An app that uploads a file to the
    specified cloud storage account blob. A message is sent to the
    downloader via the service bus and then the downloader downloads and
    removes to file from the blob.

---
+++ Downloader/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.ServiceBus;
+using Uploader.SeviceBusMessaging;
+using System.Text.RegularExpressions;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Downloader
+{
+    class Program
+    {
+        static QueueClient s_QueueClient;
+
+        static CloudStorageAccount s_Account = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("Uploader.ConnectionString"));
+        static string s_DownloadRootFolder = CloudConfigurationManager.GetSetting("Downloader.DownloadRootDir");
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            if(args.Length > 0)
+            {
+                PrintUsage();
+            }
+
+            if(!Directory.Exists(s_DownloadRootFolder))
+            {
+                Directory.CreateDirectory(s_DownloadRootFolder);
+            }
+
+            //Get Service Bus
+            var queueConnectionString =
+                CloudConfigurationManager.GetSetting("Uploader.ServiceBus.ConnectionString");
+            var queueName = CloudConfigurationManager.GetSetting("Uploader.UploadQueueName");
+            s_QueueClient = QueueClient.CreateFromConnectionString(queueConnectionString, queueName, ReceiveMode.ReceiveAndDelete);
+
+            CloudBlobClient blobClient = s_Account.CreateCloudBlobClient();
+
+            while (true)
+            {
+                try
+                {
+                    var message = s_QueueClient.Receive();
+
+                    //no new messages go back and wait again for more
+                    if(message == null)
+                    {
+                        continue;
+                    }
+
+                    EtlFileMessage etlUploadedMessage = JsonConvert.DeserializeObject<EtlFileMessage>(message.GetBody<string>());
+
+                    Console.WriteLine("Got uploaded file notification for " + etlUploadedMessage.FileName);
+
+                    // Retrieve a reference to a container. 
+                    CloudBlobContainer container = blobClient.GetContainerReference(etlUploadedMessage.ContainerName.ToLower());
+
+                    var blockBlob = container.GetBlockBlobReference(etlUploadedMessage.FileName);
+                    var localFilePath = Path.Combine(s_DownloadRootFolder, etlUploadedMessage.ContainerName.ToLower());
+                    var filePath = etlUploadedMessage.FileName.Replace("/", "\\");
+                    localFilePath = Path.Combine(localFilePath, filePath);
+
+                    if (!Directory.Exists(Path.GetDirectoryName(localFilePath)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(localFilePath));
+                    }
+
+                    if (File.Exists(localFilePath))
+                    {
+                        File.Delete(localFilePath);
+                    }
+
+                    //Get the file
+                    using (Stream cloudStoredBits = blockBlob.OpenRead())
+                    using (FileStream fs = new FileStream(localFilePath, FileMode.CreateNew, FileAccess.ReadWrite))
+                    {
+                        Console.WriteLine("Downloading Cloud file [" + etlUploadedMessage.ContainerName + "]" + etlUploadedMessage.FileName
+                            + " to " + localFilePath);
+                        cloudStoredBits.CopyTo(fs);
+                    }
+
+                    //Delete it from blob storage. Cloud storage isn't cheap :-)
+                    Console.WriteLine("Deleting Cloud file [" + etlUploadedMessage.ContainerName + "]" + etlUploadedMessage.FileName);
+                    blockBlob.Delete(DeleteSnapshotsOption.IncludeSnapshots);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Unexpected Exception occured. See this exception - " + e);
+                }
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Downloader.exe - Refer to app.config for usage ");
+        }
+
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Uploader/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.ServiceBus;
+using Uploader.SeviceBusMessaging;
+using System.Text.RegularExpressions;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System.IO;
+namespace Uploader
+{
+    class Program
+    {
+        static CloudStorageAccount s_Account = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("Uploader.Storage.ConnectionString"));
+        static CloudBlobContainer s_Container;
+        static QueueClient s_QueueClient;
+        static void Main(string[] args)
+        {
+            if(args.Length != 3)
+            {
+                PrintUsage();
+            }
+
+            try
+            {
+                
+                //Get Service Bus
+                var queueConnectionString =
+                    CloudConfigurationManager.GetSetting("Uploader.ServiceBus.ConnectionString");
+                var queueName = CloudConfigurationManager.GetSetting("Uploader.UploadQueueName");
+                s_QueueClient = QueueClient.CreateFromConnectionString(queueConnectionString, queueName);
+
+                // Create the blob client.
+                CloudBlobClient blobClient = s_Account.CreateCloudBlobClient();
+
+                // Retrieve a reference to a container. 
+                s_Container = blobClient.GetContainerReference(args[0].ToLower());
+
+                // Create the container if it doesn't already exist.
+                s_Container.CreateIfNotExists();
+
+                var localFileName = args[1];
+                var cloudFileName = args[2];
+
+                //Upload the file
+                var uploadedFileName = UploadFile(localFileName, cloudFileName);
+
+                //Notify CAP Server to download file
+                SendFileUploadedMessage(uploadedFileName, args[0].ToLower());
+            }
+            //we don't want to risk crashing on the client machines in deployment
+            catch(Exception e)
+            {
+                Console.WriteLine("Couldn't upload file: " + args[1]  + " check this execption: \n" + e);
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine(@"Uploader.exe [ContainerName] [LocalFilePath] [CloudFilePath]");
+            Console.WriteLine("[CloudPath/To/TraceFile.etl.zip");
+            Console.WriteLine();
+            Console.WriteLine("Uploads the specified file to the CAP data storage for processing. Files are uploaded securly via HTTPS");
+            Console.WriteLine("[ContainerName] is case insensitive and cannot have any spaces or punctuation");
+            Console.WriteLine("aside from a single hyphen (-)");
+            Console.WriteLine("[LocalFilePath] absolute path to local file to upload");
+            Console.WriteLine("[CloudFilePath] path");
+        }
+
+        static void SendFileUploadedMessage(string fileName, string containerName)
+        {
+            EtlFileMessage message = new EtlFileMessage()
+            {
+                FileName = fileName,
+                UploadTime = System.DateTime.UtcNow,
+                ContainerName = containerName
+            };
+
+            //not async this will block but not a big deal
+            s_QueueClient.Send(new BrokeredMessage(JsonConvert.SerializeObject(message)));
+        }
+
+        static string UploadFile(string path, string cloudPath)
+        {
+            string convertedPath;
+            //tear off any drive letters and switch slashes
+            convertedPath = cloudPath;
+            Regex r = new Regex(".*:");
+
+            if (r.IsMatch(convertedPath))
+            {
+                convertedPath = convertedPath.Replace(r.Match(convertedPath).Value, string.Empty);
+            }
+
+            //Remove root specifier if exists
+            if(convertedPath.StartsWith("/"))
+            {
+                convertedPath = convertedPath.Substring(1, convertedPath.Length - 1);
+            }
+            
+            //Remove any backslashes and replace with forward ones
+            convertedPath = convertedPath.Replace("\\", "/");
+
+            CloudBlockBlob blob = s_Container.GetBlockBlobReference(convertedPath);
+
+            // Create or overwrite the defined blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(path))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+
+            return convertedPath;
+        }
+    }
+}
